bc5e2be06f67c43a778e9cc7e3fcbc23d25f3bba
AngieLi98 <liceth5262@gmail.com>
2025-11-11 18:07:06 -0500

    laboratorio completado

--- carritoCompras/script.js
+++ carritoCompras/script.js
@@ -1,6 +1,32 @@
-function agregar(productos) {
-    let contadorProdcuto = document.getElementById(`total-${productos}`);
-    let total = parseInt(contadorProdcuto.innerText);
-    total += 1
-    contadorProdcuto.innerText = total
+function agregar(producto) {
+    let contadorProducto = document.getElementById(`total-${producto}`);
+    let precioProducto = document.getElementById(`precio-${producto}`);
+    let suma = document.getElementById("total-precio");
+
+    let cantidadProducto = parseInt(contadorProducto.innerText);
+    let precio = parseInt(precioProducto.innerText);
+    let total = parseInt(suma.innerText);
+
+    cantidadProducto += 1
+    total += precio
+
+    contadorProducto.innerText = cantidadProducto
+    suma.innerHTML = total
+
+    if(suma.innerHTML > 50){
+        alert("¡Envío gratis aplicado!")
+    }
 }
+
+function vaciar() {
+    let contadores = document.getElementsByClassName("contador");
+
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].innerText = 0;
+    }
+
+    document.getElementById("total-precio").innerText = 0;
+
+    alert("Carrito vaciado")
+}
+
